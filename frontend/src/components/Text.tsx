c774ab02af1bea07f1bfc6c3c6245953fd275b56
bruress <hualian.my.hualian@gmail.com>
2026-01-17 19:43:22 +0300

    first commit

---
+++ frontend/src/components/Text.tsx
@@ -0,0 +1,15 @@
+{/* desrcibe which props will be used */}
+interface textProps {
+    text: string;
+    type: "title" | "subtitle" | "paragraph";
+    classes?: string;
+}
+
+{/* main */}
+const Text = ({text, type, classes}:textProps) => {
+    return (
+
+    )
+}
+
+export default Text;
